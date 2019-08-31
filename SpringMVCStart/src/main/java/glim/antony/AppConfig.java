337a19243a3314017a5b7bb00192126773912e42
Antony.Glim <Antony.Glim@gmail.com>
2019-08-31 11:01:36 +0300

    AppConfig comments add

--- SpringMVCStart/src/main/java/glim/antony/AppConfig.java
+++ SpringMVCStart/src/main/java/glim/antony/AppConfig.java
@@ -12,14 +12,29 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @ComponentScan("glim.antony")
 public class AppConfig implements WebMvcConfigurer {
+
+    /**
+     * добавляет обработчик ресурсов. Метод принимает объект класса
+     * ResourceHandlerRegistry и добавляет шаблон пути и локацию. Другими словами, на все
+     * запросы /resources/** будет вызываться не контроллер, созданный разработчиком, а
+     * возвращаться указанный в запросе файл (например, .css или .js ).
+     * @param registry
+     */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**")
                 .addResourceLocations("/resources/");
     }
 
+    /**
+     * setupViewResolver() — в этом методе создается и настраивается бин, который является тем
+     * самым ViewResolver . Вспомним: контроллер возвращает только строку имени jsp-страницы, а
+     * DispatcherServlet обращается к данному бину, который формирует полный путь к
+     * представлению, прибавляя к его имени параметры, указанные в методах setPrefix и setSuffix .
+     * @return
+     */
     @Bean
-    public InternalResourceViewResolver viewResolver () {
+    public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver =new InternalResourceViewResolver();
         viewResolver.setPrefix("/WEB-INF/view/");
         viewResolver.setSuffix(".jsp");
