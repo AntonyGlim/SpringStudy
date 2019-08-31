e7d2cadcf8679a54ecc7040d4711abbbce60477b
Antony.Glim <Antony.Glim@gmail.com>
2019-08-31 11:31:10 +0300

    Controller done

--- SpringMVCStart/src/main/java/glim/antony/WelcomeController.java
+++ SpringMVCStart/src/main/java/glim/antony/WelcomeController.java
@@ -1,4 +1,18 @@
 package glim.antony;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Задача контроллера будет заключаться в передаче сообщения и возврате имени представления
+ * index.jsp . Класс контроллера будет иметь следующий вид:
+ */
+@Controller
 public class WelcomeController {
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("message", "Hello everybody!");
+        return "index";
+    }
 }
