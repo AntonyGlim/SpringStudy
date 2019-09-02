a6c757929602f9332b5059672f6114e411ed387a
Antony.Glim@gmail.com <Antony.Glim@gmail.com>
2019-09-02 19:30:00 +0300

    projects classes copied

---
+++ lesson3SpringMVCFisunov/src/main/java/glim/antony/examples/basic/EmployeeInfo.java
@@ -0,0 +1,15 @@
+package glim.antony.examples.basic;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees_info")
+public class EmployeeInfo {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    Long id;
+
+    @OneToOne(mappedBy = "info")
+    Employee employee;
+}
